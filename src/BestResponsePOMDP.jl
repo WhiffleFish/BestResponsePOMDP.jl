b837130ee5f49721d8b9ab2362e027909a920128
WhiffleFish <tylertronics@gmail.com>
2022-08-07 11:47:04 -0400

    init deps

---
+++ src/BestResponsePOMDP.jl
@@ -0,0 +1,10 @@
+module BestResponsePOMDP
+
+using CounterfactualRegret
+const CFR = CounterfactualRegret
+using POMDPs
+using POMDPTools
+using ParticleFilters
+greet() = print("Hello World!")
+
+end # module
